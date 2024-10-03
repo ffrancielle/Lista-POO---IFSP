904a59d296b0470f13bc85cfe67b550fb582e252
ffrancielle <165734193+ffrancielle@users.noreply.github.com>
2024-10-03 18:27:16 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projPOO_ex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Quadrado quadradinho = new Quadrado();
+
+            Console.WriteLine("Digite o valor da aresta do quadrado: ");
+            int aresta = int.Parse(Console.ReadLine());
+
+            quadradinho.SetAresta(aresta);
+            quadradinho.CalcularA();
+
+            Console.WriteLine("A área é: " + quadradinho.GetArea());
+            Console.ReadLine();
+        }
+    }
+}
